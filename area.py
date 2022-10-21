372430912ac6c53cd884f626376a58e1fb88ff72
Tanushi09 <tznushio>
2022-10-21 18:26:11 +0530

    loops

---
+++ area.py
@@ -0,0 +1,6 @@
+c=float(input("enter circumference"))
+pi=22/7
+r=7*c/44
+print("radius:",r)
+area=pi*r*r
+print('area:',area )
---
+++ si.py
@@ -0,0 +1,5 @@
+p=float(input('enter principle amount'))
+r=float(input('enter rate of interest'))
+t=float(input('enter time'))
+si=(p*t*r)/100
+print(si)
